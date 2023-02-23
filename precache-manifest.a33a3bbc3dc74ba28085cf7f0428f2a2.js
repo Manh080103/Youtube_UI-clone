97a1d54a7d58c75d68ddd688a256d9d38c7f21cf
Manh Nguyen <ducmanh080103@gmail.com>
2023-02-23 22:46:06 +0700

    Updates

---
+++ precache-manifest.a33a3bbc3dc74ba28085cf7f0428f2a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9789ebc4560701efd00",
+    "url": "/static/css/main.e262db03.chunk.css"
+  },
+  {
+    "revision": "e9789ebc4560701efd00",
+    "url": "/static/js/main.e9789ebc.chunk.js"
+  },
+  {
+    "revision": "bdb52dcc075e89110491",
+    "url": "/static/js/1.bdb52dcc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ae6a62171cdca835f4419f55cc167e3",
+    "url": "/static/media/logo-white.8ae6a621.png"
+  },
+  {
+    "revision": "1687f228b10f04f9b46c309a0fdfc665",
+    "url": "/static/media/youtubeBanner.1687f228.png"
+  },
+  {
+    "revision": "0e432fcfe2905d8ad9827475593c1ad4",
+    "url": "/static/media/youtubeLogo.0e432fcf.png"
+  },
+  {
+    "revision": "2a9dc3ae6e4b5dd234bdf17249d19934",
+    "url": "/index.html"
+  }
+];
